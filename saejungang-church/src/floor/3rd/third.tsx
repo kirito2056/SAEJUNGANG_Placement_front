b50a7257d763b0462e58f296c068301339a21908
하진 권씨 <hajin_2056@naver.com>
2025-05-10 01:33:52 +0900

    DELETE :: 층계 알려주는 h1 태그

--- saejungang-church/src/floor/3rd/third.tsx
+++ saejungang-church/src/floor/3rd/third.tsx
@@ -4,7 +4,6 @@ import './third.css';
 const Floor3Page: React.FC = () => {
   return (
     <div className="floor2-container">
-      <h1>3층</h1>
       <div className="seats-container">
         <div className="seat">3층 좌석 1</div> 
         <div className="seat">3층 좌석 2</div>
